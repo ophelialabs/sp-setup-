6ff3316ed7f5bd95efa752da6913a73592dee981
jlab <jlabcloud@gmail.com>
2025-12-13 20:58:56 -0500

    update need to add sitenav

--- FlaskJinjaPy/views.py
+++ FlaskJinjaPy/views.py
@@ -36,11 +36,11 @@ def about():
         message='Your application description page.'
     )
 
-@app.route('/admindashboard.html')
+@app.route('/pages/admindashboard')
 def admindashboard():
     """Renders the admin dashboard page."""
     return render_template(
-        'admindashboard.html',
+        'pages/admindashboard.html',
         title='Admin Dashboard',
         year=datetime.now().year,
         message='Admin dashboard overview.'
@@ -56,7 +56,7 @@ def profile():
         message='Your user profile page.'
     )
 
-@app.route('/partials/_messages.html')
+@app.route('/partials/_messages')
 def messages():
     """Renders the messages partial."""
     return render_template(
@@ -64,4 +64,68 @@ def messages():
         title='Messages Partial',
         year=datetime.now().year,
         message='This is a messages partial.'
+    )
+
+"""
+Routes and views for AppBar Global Navigation
+"""
+
+@app.route('/pages/globalnav/news')
+def news():
+    """Renders the news page."""
+    return render_template(
+        'pages/globalnav/news.html',
+        title='News',
+        year=datetime.now().year,
+        message='Latest news and updates.'
+    )
+
+@app.route('/pages/globalnav/connections')
+def connections():
+    """Renders the connections page."""
+    return render_template(
+        'pages/globalnav/connections.html',
+        title='Connections',
+        year=datetime.now().year,
+        message='Your connections page.'
+    )
+
+@app.route('/files')
+def files():
+    """Renders the files page."""
+    return render_template(
+        'pages/globalnav/files.html',
+        title='Files',
+        year=datetime.now().year,
+        message='Your files page.'
+    )
+
+@app.route('/research')
+def research():
+    """Renders the research page."""
+    return render_template(
+        'pages/globalnav/research.html',
+        title='Research',
+        year=datetime.now().year,
+        message='Your research page.'
+    )
+
+@app.route('/sites')
+def sites():
+    """Renders the sites page."""
+    return render_template(
+        'pages/globalnav/sites.html',
+        title='Sites',
+        year=datetime.now().year,
+        message='Your sites page.'
+    )
+
+@app.route('/lists')
+def lists():
+    """Renders the lists page."""
+    return render_template(
+        'pages/globalnav/lists.html',
+        title='Lists',
+        year=datetime.now().year,
+        message='Your lists page.'
     )
